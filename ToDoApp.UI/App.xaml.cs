6be21d150b68b5a082739cd2149467e119ab749b
Dylan King <dylanrichardking@gmail.com>
2022-05-10 17:00:42 +0100

    Basic window added - Included MDIX - Temp code behind for window fucntionality (might leave this here as it's specific to desktop UI)

--- ToDoApp.UI/App.xaml.cs
+++ ToDoApp.UI/App.xaml.cs
@@ -13,5 +13,6 @@ namespace ToDoApp.UI
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
---
+++ ToDoApp.UI/Views/WindowShellView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToDoApp.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for WindowShellView.xaml
+    /// </summary>
+    public partial class WindowShellView : Window
+    {
+        public WindowShellView()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowMinimiseButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void WindowMaximiseButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+            WindowMaximiseButton.Visibility = Visibility.Collapsed;
+            WindowRestoreButton.Visibility = Visibility.Visible;
+        }
+
+        private void WindowRestoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Normal;
+            WindowRestoreButton.Visibility = Visibility.Collapsed;
+            WindowMaximiseButton.Visibility = Visibility.Visible;
+        }
+
+        private void WindowCloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
