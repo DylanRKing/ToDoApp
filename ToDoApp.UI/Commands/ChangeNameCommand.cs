7552dc0c6d41594b9c31b346e9c1ed412145f2e5
Dylan King <dylanrichardking@gmail.com>
2022-05-16 23:11:53 +0100

    Sanity Check and Testing Expanders in a new window

---
+++ ToDoApp.UI/Commands/ChangeNameCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ToDoApp.UI.ViewModels;
+
+namespace ToDoApp.UI.Commands
+{
+    public class ChangeNameCommand : ICommand
+    {
+        private WindowShellViewModel vm { get; set; }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public ChangeNameCommand(WindowShellViewModel vM)
+        {
+            vm = vM;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            vm.ChangeWindowName();
+        }
+    }
+}
---
+++ ToDoApp.UI/ViewModels/WindowShellViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ToDoApp.UI.Commands;
+
+namespace ToDoApp.UI.ViewModels
+{
+    public class WindowShellViewModel : MasterViewModel
+    {
+
+        private string windowTitle = "To-Do App";
+
+        public string WindowTitle
+        {
+            get { return windowTitle; }
+            set 
+            { 
+                windowTitle = value;
+                OnPropertyChanged("WindowTitle");
+            }
+        }
+
+        public ChangeNameCommand cNameCommand { get; set; }
+
+        public WindowShellViewModel()
+        {
+            cNameCommand = new ChangeNameCommand(this);
+        }
+
+        public void ChangeWindowName()
+        {
+            WindowTitle = "Testing";
+        }
+    }
+}
